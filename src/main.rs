94bfbcdd11d61bac090301618e71de7ad4642e28
Sean Chen <skypemaster007@gmail.com>
2020-06-17 22:32:32 -0700

    Minimal working example of replacing a let binding

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
-use std::env;
-use std::fs::File;
-use std::io::Read;
-use std::process;
+use quote::quote;
+use proc_macro2::{Ident, Span};
+use syn::visit_mut::{self, VisitMut};
+use syn::{File, PatIdent};
+
+struct LetBindingReplace;
+
+impl VisitMut for LetBindingReplace {
+    fn visit_pat_ident_mut(&mut self, node: &mut PatIdent) {
+        *node = PatIdent {
+            attrs: vec![],
+            by_ref: node.by_ref,
+            mutability: node.mutability,
+            ident: Ident::new("PLACEHOLDER", Span::call_site()),
+            subpat: None,
+        };
+        
+        visit_mut::visit_pat_ident_mut(self, node);
+    }
+}
 
 fn main() {
-    let mut args = env::args();
-    let _ = args.next(); // executable name
-
-    let filename = match (args.next(), args.next()) {
-        (Some(filename), None) => filename,
-        _ => {
-            eprintln!("Usage: dump-syntax path/to/filename.rs");
-            process::exit(1);
+    let code = quote! {
+        fn main() {
+            let x = 5;
+            let _ = 999u256;
         }
     };
 
-    let mut file = File::open(&filename).expect("Unable to open file");
-
-    let mut src = String::new();
-    file.read_to_string(&mut src).expect("Unable to read file");
-
-    let syntax = syn::parse_file(&src).expect("Unable to parse file");
-    println!("{:#?}", syntax);
+    let mut syntax_tree: File = syn::parse2(code).unwrap();
+    LetBindingReplace.visit_file_mut(&mut syntax_tree);
+    println!("{}", quote!(#syntax_tree));
 }
