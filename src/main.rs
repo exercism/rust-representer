bfb333f505f999b6525d0f70a3b5d67d96c88f38
Remo Senekowitsch <remo@buenzli.dev>
2023-07-27 15:30:05 +0200

    Update clap from 2.x to 4.x

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
-use clap::{App, Arg, ArgMatches};
+use clap::{Arg, ArgMatches, Command};
 use rust_representer::run;
 use std::process;
 
-fn init_app<'a>() -> ArgMatches<'a> {
-    App::new(env!("CARGO_PKG_NAME"))
+fn init_app() -> ArgMatches {
+    Command::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
-            Arg::with_name("slug")
-                .short("s")
+            Arg::new("slug")
+                .short('s')
                 .long("slug")
                 .help("The slug of the exercise to be analyzed (e.g. 'reverse-string').")
-                .takes_value(true)
                 .required(true),
         )
         .arg(
-            Arg::with_name("path")
-                .short("p")
+            Arg::new("path")
+                .short('p')
                 .long("path")
                 .help("A path to a directory containing the submitted file(s).")
-                .takes_value(true)
                 .required(true),
         )
         .get_matches()
@@ -27,7 +25,7 @@ fn init_app<'a>() -> ArgMatches<'a> {
 
 fn main() {
     let matches = init_app();
-    let path = format!("{}src/lib.rs", matches.value_of("path").unwrap());
+    let path = format!("{}src/lib.rs", matches.get_one::<String>("path").unwrap());
 
     if let Err(error) = run(&path) {
         eprintln!("[error] {}", error);
