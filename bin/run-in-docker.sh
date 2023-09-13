8af0080d9286eb8d9484514e2f4be21463246eff
AurelienFT <aurelien.foucault@epitech.eu>
2023-09-13 17:26:58 +0200

    Update run in docker script to match production criteria

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -1,24 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 
 # Synopsis:
-# Analysis runner for run.sh in a docker container
-# Takes the same arguments as run.sh (EXCEPT THAT SOLUTION AND OUTPUT PATH ARE RELATIVE)
-# Builds the Dockerfile
-# Runs the docker image passing along the initial arguments
+# Run the representer on a solution using the representer Docker image.
+# The representer Docker image is built automatically.
 
 # Arguments:
 # $1: exercise slug
-# $2: **RELATIVE** path to solution folder (with trailing slash)
-# $3: **RELATIVE** path to output directory (with trailing slash)
+# $2: absolute path to solution folder
+# $3: absolute path to output directory
 
 # Output:
-# Writes the analyasis results to a analyasis.json file in the passed-in output directory.
-# The results are formatted according to the specifications at https://github.com/exercism/docs/blob/main/building/tooling/analyzers/interface.md
+# Writes the test results to a results.json file in the passed-in output directory.
+# The test results are formatted according to the specifications at https://github.com/exercism/docs/blob/main/building/tooling/representers/interface.md
 
 # Example:
-# ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder/ ./relative/path/to/output/directory/
-
+# ./bin/run-in-docker.sh two-fer /absolute/path/to/two-fer/solution/folder/ /absolute/path/to/output/directory/
 # If arguments not provided, print usage and exit
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     echo "usage: run-in-docker.sh exercise-slug ./relative/path/to/solution/folder/ ./relative/path/to/output/directory/"
@@ -26,7 +23,7 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
 fi
 
 # build docker image
-docker build --platform linux/amd64 --rm --no-cache -t rust-representer .
+docker build --rm --no-cache -t exercism/rust-representer .
 
 # Create output directory if it doesn't exist
 output_dir="$3"
@@ -34,6 +31,10 @@ mkdir -p "$output_dir"
 
 # run image passing the arguments
 docker run \
-    --mount type=bind,src=$PWD/$2,dst=/solution \
-    --mount type=bind,src=$PWD/$output_dir,dst=/output \
-    rust-representer $1 /solution/ /output/
+    --rm \
+    --network none \
+    --read-only \
+    --mount type=bind,source="${PWD}/${2}",destination=/solution \
+    --mount type=bind,source="${PWD}/${output_dir}",destination=/output \
+    --mount type=tmpfs,destination=/tmp \
+    exercism/rust-representer $1 /solution/ /output/
