2a9c8f59812e9556f516c0b1030904fe56b49c38
Erik Schierboom <erik_schierboom@hotmail.com>
2023-10-31 12:37:34 +0100

    Add expected files

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -14,11 +14,11 @@
 exit_code=0
 
 # Iterate over all test directories
-for test_dir in tests/*; do
+for test_dir in $(find tests -mindepth 1 -maxdepth 1 -type d); do
     test_dir_name=$(basename "${test_dir}")
     test_dir_path=$(realpath "${test_dir}")
 
-    bin/run.sh "${test_dir_name}" "${test_dir_path}" "${test_dir_path}"
+    bin/run.sh "${test_dir_name}" "${test_dir_path}/" "${test_dir_path}/"
 
     for file in representation.txt representation.json mapping.json; do
         expected_file="expected_${file}"
